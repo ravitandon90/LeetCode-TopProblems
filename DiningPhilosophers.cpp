1bdf0dcc52a84938989fd8ce75ceaa2ef81ef84a
Ravi Tandon <ravi@decoverhq.com>
2022-07-23 17:54:52 +0000

    first commit

---
+++ DiningPhilosophers.cpp
@@ -0,0 +1,50 @@
+// Problem: https://leetcode.com/problems/the-dining-philosophers/
+// Dining Philosophers
+
+#include<vector>
+#include<mutex>
+#include<condition_variable>
+#include<functional>
+
+
+class DiningPhilosophers {
+public:
+    DiningPhilosophers() {
+        spoons.resize(NUM_PHILOSOPHERS, true);
+    }
+
+    void wantsToEat(int philosopher,
+                    function<void()> pickLeftFork,
+                    function<void()> pickRightFork,
+                    function<void()> eat,
+                    function<void()> putLeftFork,
+                    function<void()> putRightFork) {
+        
+        {
+            std::unique_lock<std::mutex> lock(mtx_);
+            cond.wait(lock, [this, philosopher] {
+                return (spoons[philosopher] && spoons[(philosopher + 1)% NUM_PHILOSOPHERS]);
+            });
+            spoons[philosopher] = false;
+            spoons[(philosopher + 1) % NUM_PHILOSOPHERS] = false;
+            pickLeftFork();
+            pickRightFork();
+            
+        }
+        eat();
+        {
+            std::unique_lock<std::mutex> lock(mtx_);
+            spoons[philosopher] = true;
+            spoons[(philosopher + 1) % NUM_PHILOSOPHERS] = true;
+            putLeftFork();
+            putRightFork();
+            cond.notify_all();
+        }		
+    }
+    
+    private:
+    vector<bool> spoons;
+    int NUM_PHILOSOPHERS = 5;
+    std::mutex mtx_;
+    std::condition_variable cond;
+};
---
+++ HashTable/HitCounter.cpp
@@ -0,0 +1,37 @@
+#include<vector>
+
+using namespace std;
+
+class HitCounter {
+public:
+    HitCounter() {
+        times.resize(300, 0);
+        hits.resize(300, 0);
+    }
+    
+    void hit(int timestamp) {
+        int t_mod = timestamp % 300;
+        if (times[t_mod] != timestamp) {
+            times[t_mod] = timestamp;
+            hits[t_mod] = 1;
+        } else {
+            hits[t_mod]++;
+        }
+        
+    }
+    
+    int getHits(int timestamp) {
+        int sum = 0;
+        for (int i = 0; i < 300; ++i) {
+            int t_delta = timestamp - times[i];
+            if (t_delta < 300) {
+                sum += hits[i];
+            }
+        }
+        return sum;
+    }
+
+private:
+    vector<int> times;
+    vector<int> hits;
+};
